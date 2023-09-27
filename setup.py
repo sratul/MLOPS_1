c8dc81fec07ef9bdd2e4770c43efbe801b16bb4b
sratul <36583516+sratul@users.noreply.github.com>
2023-09-27 14:35:38 -0400

    Github And Code Set Up

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+HYPHEN_E = '-e .'
+
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+        This function will return a list of requirements
+    '''
+
+    requirements = []
+
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+
+        if HYPHEN_E in requirements:
+            requirements.remove(HYPHEN_E)
+
+    return requirements
+
+
+
+setup(
+    name = 'mlproject',
+    version = '0.0.1',
+    author = 'Saiful',
+    author_email= 'saiful_ratul@outlook.com',
+    packages =find_packages(),
+    install_requires = get_requirements('requirements.txt'), 
+
+)
