8e906279d957c6fecf3a093bb8ab3882af6404b4
sratul <36583516+sratul@users.noreply.github.com>
2023-10-01 23:19:23 -0400

    Created Prediction Pipeline Using Flask Web APP

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -50,7 +50,42 @@ class ModelTrainer:
                 "AdaBoost Classifier": AdaBoostRegressor(),
             }
 
-            model_report:dict=evaluate_models(X_train=X_train,y_train=   y_train,X_test=X_test,y_test=y_test,models=models)
+            params = {
+                "Random Forest": {
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Decision Tree":{
+                    'criterion':['squared_error','friedman_mse','absolute_error','poisson'],
+                },
+                "Gradient Boosting": {
+                    'learning_rate':[0.1,0.01,0.005,0.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Linear Regression": {},
+
+                "K-Neighbour Regressor":{
+                        'n_neighbors':[5,7,9,11],
+                },
+                "XGBClassifer": {
+                    'learning_rate':[0.1,0.01,0.005,0.001],
+                    # 'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators':[8,16,32,64,128,256],
+                },
+                "CatBoosting Classifer": {
+                    'depth': [6,8,10],
+                    'learning_rate':[0.1,0.01,0.005],
+                    'iterations': [30,50,100]
+                },
+                "AdaBoost Classifier": {
+                     'learning_rate':[0.1,0.01,0.005,0.001],
+                    # 'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators':[8,16,32,64,128,256],
+                },
+            }
+
+
+            model_report:dict=evaluate_models(X_train=X_train,y_train=   y_train,X_test=X_test,y_test=y_test,models=models,params=params)
         
             best_model_score = max(sorted(model_report.values()))
 
